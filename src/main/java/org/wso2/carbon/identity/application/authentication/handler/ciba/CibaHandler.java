5b085a3037403c5ec153dc90c827334c966b5edf
VIvek <cvivekvinushanth@gmail.com>
2019-09-04 13:56:13 +0530

    Edited.

--- src/main/java/org/wso2/carbon/identity/application/authentication/handler/ciba/CibaHandler.java
+++ src/main/java/org/wso2/carbon/identity/application/authentication/handler/ciba/CibaHandler.java
@@ -5,7 +5,9 @@ import net.minidev.json.parser.ParseException;
 import org.wso2.carbon.identity.application.authentication.framework.AbstractApplicationAuthenticator;
 import org.wso2.carbon.identity.application.authentication.framework.AuthenticationFlowHandler;
 import org.wso2.carbon.identity.application.authentication.framework.AuthenticatorFlowStatus;
+import org.wso2.carbon.identity.application.authentication.framework.LocalApplicationAuthenticator;
 import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
+import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedIdPData;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Username Password based Authenticator.
  */
-public class CibaHandler extends AbstractApplicationAuthenticator implements AuthenticationFlowHandler {
+public class CibaHandler extends AbstractApplicationAuthenticator implements LocalApplicationAuthenticator {
 
     private static final long serialVersionUID = -4406878411547612129L;
     // private static final Log log = LogFactory.getLog(AbstractApplicationAuthenticator.class);
@@ -24,30 +26,30 @@ public class CibaHandler extends AbstractApplicationAuthenticator implements Aut
     @Override
     public AuthenticatorFlowStatus process(HttpServletRequest request,
                                            HttpServletResponse response, AuthenticationContext context) {
+       // AuthenticatedIdPData local = context.getPreviousAuthenticatedIdPs().get(BasicAuthenticatorConstants.LOCAL);
         if (context.isLogoutRequest()) {
             return AuthenticatorFlowStatus.SUCCESS_COMPLETED;
-        } else {
-            // Get username from the request and set to the context.
+        } else{
+            /*// Get username from the request and set to the context.
             String stateparameter = request.getParameter("state");
             try {
                 JSONObject json = (JSONObject) new net.minidev.json.parser.JSONParser().parse(stateparameter);
-                String username = String.valueOf(json.get("username"));
+                String username = String.valueOf(json.get("username"));*/
 
                 AuthenticatedUser authenticatedUser = AuthenticatedUser.
-                        createLocalAuthenticatedUserFromSubjectIdentifier(username);
+                        createLocalAuthenticatedUserFromSubjectIdentifier("admin");
+
+            /*AuthenticatedUser authenticatedUser2 = AuthenticatedUser.
+                    createFederateAuthenticatedUserFromSubjectIdentifier("admin");*/
                 context.setSubject(authenticatedUser);
+                //context.setSubject(authenticatedUser2);
                 return AuthenticatorFlowStatus.SUCCESS_COMPLETED;
 
-            } catch (ParseException e) {
-                e.printStackTrace();
             }
 
             //JSONObject json = ( JSONObject) new JSONParser().parse(stateparameter);
             // JSONObject json = (JSONObject) parser.parse(stateparameter);
 
-
-        }
-        return AuthenticatorFlowStatus.INCOMPLETE;
     }
 
 
